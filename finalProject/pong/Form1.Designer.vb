20cc7a9c071c23ecf2661a436bfbdfd3fa785f9d
Dusty <dbeehler@users.noreply.github.com>
2017-12-12 14:32:39 -0600

    first version

---
+++ finalProject/pong/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picBallMain = New System.Windows.Forms.PictureBox()
+        Me.picPaddle2 = New System.Windows.Forms.PictureBox()
+        Me.picPaddle1 = New System.Windows.Forms.PictureBox()
+        CType(Me.picBallMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPaddle2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPaddle1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBallMain
+        '
+        Me.picBallMain.Image = Global.pong.My.Resources.Resources.ball
+        Me.picBallMain.Location = New System.Drawing.Point(372, 176)
+        Me.picBallMain.Name = "picBallMain"
+        Me.picBallMain.Size = New System.Drawing.Size(24, 24)
+        Me.picBallMain.TabIndex = 2
+        Me.picBallMain.TabStop = False
+        '
+        'picPaddle2
+        '
+        Me.picPaddle2.Image = Global.pong.My.Resources.Resources.Paddle
+        Me.picPaddle2.Location = New System.Drawing.Point(862, 12)
+        Me.picPaddle2.Name = "picPaddle2"
+        Me.picPaddle2.Size = New System.Drawing.Size(10, 50)
+        Me.picPaddle2.TabIndex = 1
+        Me.picPaddle2.TabStop = False
+        '
+        'picPaddle1
+        '
+        Me.picPaddle1.Image = Global.pong.My.Resources.Resources.Paddle
+        Me.picPaddle1.Location = New System.Drawing.Point(12, 12)
+        Me.picPaddle1.Name = "picPaddle1"
+        Me.picPaddle1.Size = New System.Drawing.Size(10, 50)
+        Me.picPaddle1.TabIndex = 0
+        Me.picPaddle1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(884, 412)
+        Me.Controls.Add(Me.picBallMain)
+        Me.Controls.Add(Me.picPaddle2)
+        Me.Controls.Add(Me.picPaddle1)
+        Me.Name = "Form1"
+        Me.Text = "Pong"
+        CType(Me.picBallMain, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPaddle2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPaddle1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents picPaddle1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picPaddle2 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBallMain As System.Windows.Forms.PictureBox
+
+End Class
---
+++ finalProject/pong/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Dim previousKey As Keys? = Nothing
+    Const speedOfBall As Double = 10.5
+    Dim angleOfBall As Double = 0.0
+    Dim previousAngle As Double = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CenterToScreen()
+        Me.Size = New Size(900, 450)
+    End Sub
+
+    Private Sub paddle1Move(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.W Then
+            picPaddle1.Top -= 10
+            If picPaddle1.Top < 10 Then
+                picPaddle1.Top = 12
+            End If
+        ElseIf e.KeyCode = Keys.S Then
+            picPaddle1.Top += 10
+            If picPaddle1.Top > 350 Then
+                picPaddle1.Top = 350
+            End If
+        End If
+        If e.KeyCode = Keys.Y Then
+            picBallMain.Left += 10
+        End If
+    End Sub
+
+    Private Sub paddle2Move(ByVal sender As System.Object, ByVal d As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If d.KeyCode = Keys.Up Then
+            picPaddle2.Top -= 10
+            If picPaddle2.Top < 10 Then
+                picPaddle2.Top = 12
+            End If
+        ElseIf d.KeyCode = Keys.Down Then
+            picPaddle2.Top += 10
+            If picPaddle2.Top > 350 Then
+                picPaddle2.Top = 350
+            End If
+        End If
+    End Sub
+
+    Private Sub collision()
+        angleOfBall = previousAngle
+        If picBallMain.Bounds.IntersectsWith(picPaddle1.Bounds) Then
+            angleOfBall = previousAngle - 1
+            picBallMain.Top -= angleOfBall
+            picBallMain.Left -= speedOfBall
+        ElseIf picBallMain.Bounds.IntersectsWith(picPaddle2.Bounds) Then
+            angleOfBall = previousAngle + 1
+            picBallMain.Top += angleOfBall
+            picBallMain.Left += speedOfBall
+        End If
+        If picBallMain.Top < 10 Then
+            picBallMain.Top = 10
+        ElseIf picBallMain.Top > 350 Then
+            picBallMain.Top = 350
+        End If
+    End Sub
+End Class
